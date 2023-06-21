0fc61099a51b2a8fedc4ab05e4d7bb5986e788eb
PedroCrosara <47993775+PedroCrosara@users.noreply.github.com>
2023-06-21 14:29:21 -0300

    Update config.py
    file path

--- covid_webV2/config.py
+++ covid_webV2/config.py
@@ -1,7 +1,7 @@
 
 def get_lung_segmentation_ckpt(): 
-    return '/home/pedro/Desktop/Mestrado/covid_webV2/weights/lung-paper-mobilenet_v2-FPN-1-2632-250.ckpt'   
+    return './covid_webV2/weights/lung-paper-mobilenet_v2-FPN-1-2632-250.ckpt'   
 def get_lesion_segmentation_ckpt(): 
-    return '/home/pedro/Desktop/Mestrado/covid_webV2/weights/lung-paper-mobilenet_v2-FPN-10-5109-250.ckpt'
+    return './covid_webV2/weights/lung-paper-mobilenet_v2-FPN-10-5109-250.ckpt'
 def get_classification_ckpt(): 
-    return '/home/pedro/Desktop/Mestrado/covid_webV2/weights/lung-paper-mobilenet_v2-FPN-1-2632-250.ckpt'
+    return './covid_webV2/weights/covidxct-densenet-6762.ckpt'
